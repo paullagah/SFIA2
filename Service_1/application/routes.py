b98df4870ce3683971b90822925c78c18e679db6
paullagah <plagah@outlook.com>
2020-07-22 14:27:31 +0100

    SFIA 2 Folder/File Structure

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -1,9 +1,8 @@
 from application import app
+from flask import render_template, request
+import requests
+import random
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
