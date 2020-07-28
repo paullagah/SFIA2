15513d4d2bf7dcf3680a0ae3e5c2d094d73feb45
Paul Lagah <plagah93@gmail.com>
2020-07-28 15:30:40 +0000

    Docker push in new image build fixed for each service

--- scripts/newimagebuild.sh
+++ scripts/newimagebuild.sh
@@ -2,13 +2,13 @@
 
 docker build -t paullagah/service_1 ./Service_1
 docker tag paullagah/service_1:latest paullagah/service_1:update
-docker push paullagah/service_4:update
+docker push paullagah/service_1:update
 docker build -t paullagah/service_2 ./Service_2
 docker tag paullagah/service_2:latest paullagah/service_2:update
-docker push paullagah/service_4:update
+docker push paullagah/service_2:update
 docker build -t paullagah/service_3 ./Service_3
 docker tag paullagah/service_3:latest paullagah/service_3:update
-docker push paullagah/service_4:update
+docker push paullagah/service_3:update
 docker build -t paullagah/service_4 ./Service_4
 docker tag paullagah/service_4:latest paullagah/service_4:update 
 docker push paullagah/service_4:update
