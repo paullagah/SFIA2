eb99244c466cb0f5f87cb1b9a4e18016e74c0327
Paul Lagah <plagah93@gmail.com>
2020-07-28 10:31:07 +0000

    removed double dots in services

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,20 +2,20 @@
 
 # Build Service 1 image if it doesn't exist
 if [[ "$(docker images -q paullagah/service_1:latest 2> /dev/null)" == "" ]]; then
-    docker build -t paullagah/service_1 ../Service_1
+    docker build -t paullagah/service_1 ./Service_1
 fi
 
 # Build Service 2 image if it doesn't exist
-if [[ "$(docker images -q paullagah/service_1:latest 2> /dev/null)" == "" ]]; then
-docker build -t paullagah/service_1 ../Service_1
+if [[ "$(docker images -q paullagah/service_2:latest 2> /dev/null)" == "" ]]; then
+docker build -t paullagah/service_2 ./Service_2
 fi
 
 # Build Service 3 image if it doesn't exist
-if [[ "$(docker images -q paullagah/service_1:latest 2> /dev/null)" == "" ]]; then
-docker build -t paullagah/service_1 ../Service_1
+if [[ "$(docker images -q paullagah/service_3:latest 2> /dev/null)" == "" ]]; then
+docker build -t paullagah/service_3 ./Service_3
 fi
 
 # Build Service 4 image if it doesn't exist
-if [[ "$(docker images -q paullagah/service_1:latest 2> /dev/null)" == "" ]]; then
-docker build -t paullagah/service_1 ../Service_1
+if [[ "$(docker images -q paullagah/service_4:latest 2> /dev/null)" == "" ]]; then
+docker build -t paullagah/service_4 ./Service_4
 fi
