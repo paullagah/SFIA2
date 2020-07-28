34714a3e5f60107f742421792e09d2ffcec8be02
Paul Lagah <plagah93@gmail.com>
2020-07-28 11:20:50 +0000

    deploy stack script now checks if stack is running and stops stack and pushes rolling update before restarting stack

--- scripts/deploy_stack.sh
+++ scripts/deploy_stack.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+if [[ "$(docker stack ls 2> /dev/null)" == "" ]]; then
 docker stack deploy --compose-file docker-compose.yaml DnDservice
+fi
+
+if [[ "$(docker stack ls 2> /dev/null)" == "DnDservice" ]]; then
+docker stack rm DnDservice
+docker service update --image paullagah/service_1:latest DnDservice_service_1
+docker service update --image paullagah/service_2:latest DnDservice_service_2
+docker service update --image paullagah/service_3:latest DnDservice_service_3
+docker service update --image paullagah/service_4:latest DnDservice_service_4
+docker stack deploy --compose-file docker-compose.yaml DnDservice
+fi
