f62281516d9d62db3466be8655dbecf2854a0f1e
Paul Lagah <plagah93@gmail.com>
2020-07-29 14:56:26 +0000

    updated environment variables

--- Service_4/application/routes.py
+++ Service_4/application/routes.py
@@ -7,15 +7,15 @@ import random
 app.config['SECRET_KEY'] = '919c92fab903330df5b2f66c22d3b22b'  # environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
-                                        environ.get('SQLHOST') + \
+                                        environ.get('MYSQL_HOST') + \
                                         ':' + \
-                                        environ.get('PASSWORD') + \
+                                        environ.get('MYSQL_PASSWORD') + \
                                         '@' + \
-                                        environ.get('USER') + \
+                                        environ.get('MYSQL_PORT') + \
                                         ':' + \
-                                        environ.get('PORT') + \
+                                        environ.get('MYSQL_USER') + \
                                         '/' + \
-                                        environ.get('DBNAME')
+                                        environ.get('MYSQL_DB_NAME')
 
 db = SQLAlchemy(app)
 
