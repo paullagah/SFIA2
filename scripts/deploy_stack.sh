cd73eef5fdcf709eadf04a878727d8daaad6e48d
Paul Lagah <plagah93@gmail.com>
2020-07-29 09:05:09 +0000

    implemented new image build with no cache and performs rolling update, clearing old images

--- scripts/deploy_stack.sh
+++ scripts/deploy_stack.sh
@@ -5,8 +5,13 @@ docker stack deploy --compose-file docker-compose.yaml DnDservice
 fi
 
 if [[ "$(docker stack ls 2> /dev/null)" == "DnDservice" ]]; then
+docker push paullagah/service_1:latest
 docker service update --image paullagah/service_1:latest DnDservice_service_1
+docker push paullagah/service_2:latest
 docker service update --image paullagah/service_2:latest DnDservice_service_2
+docker push paullagah/service_3:latest
 docker service update --image paullagah/service_3:latest DnDservice_service_3
+docker push paullagah/service_4:latest
 docker service update --image paullagah/service_4:latest DnDservice_service_4
+docker rmi $(docker images -f "dangling=true" -q) -f
 fi
