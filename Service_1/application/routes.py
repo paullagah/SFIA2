dd2b8a6308df0292a3bb6fb0b4af126c501a4ec8
Paul Lagah <plagah93@gmail.com>
2020-07-29 13:56:26 +0000

    Created new database and table to persist data

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -1,11 +1,31 @@
 from application import app
 from flask import render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import requests
 import random
+
+
+db = SQLAlchemy(app)
+
+class dnd(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    races = db.Column(db.String(50), nullable=False)
+    classes = db.Column(db.String(50), nullable=False)
+   
+    def __repr__(self):
+    return ''.join(
+        [
+            'Race: ' + self.races + '\n' 
+            'Classes ' + self.classes + '\n'
+            'ID: ' + str(self.id) 
+        ]
+    )
+
 
 @app.route('/', methods=['GET'])
 def home():
     response = requests.get('http://service_4:5003/merge')
     print(response)
     sentence = response.text
-    return render_template('index.html', sentence = sentence, title = 'Home')
+    dnd_data = dnd.query.all()
+    return render_template('index.html', sentence = sentence, dnd = dnd_data, title = 'Home')
--- Service_4/application/routes.py
+++ Service_4/application/routes.py
@@ -1,11 +1,35 @@
 from application import app
 from flask import render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import requests
 import random
 
-@app.route('/merge', methods=['GET'])
+db = SQLAlchemy(app)
+
+class dnd(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    races = db.Column(db.String(50), nullable=False)
+    classes = db.Column(db.String(50), nullable=False)
+   
+    def __repr__(self):
+    return ''.join(
+        [
+            'Race: ' + self.races + '\n' 
+            'Classes ' + self.classes + '\n'
+            'ID: ' + str(self.id) 
+        ]
+    )
+
+@app.route('/merge', methods=['GET', 'POST'])
 def merge():
     race = requests.get('http://service_2:5001/race')
     classes = requests.get('http://service_3:5002/classes')
     response = "Your race is " + race.text + " and your class is " + classes.text
+    dnd_data = dnd(
+            races=race
+            classes=classes
+        )
+        db.session.add(dnd_data)
+        db.session.commit()
+    
     return response
