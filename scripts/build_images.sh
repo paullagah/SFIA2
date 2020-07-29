b493a62d6581421fa8a032311171a6bf04092ebe
Paul Lagah <plagah93@gmail.com>
2020-07-29 09:07:44 +0000

    updated image build with "--no-cache" tags

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,13 +2,13 @@
 
 # Print image list before build -> Build Service 1 image 
 echo $(docker images)
-docker build -t paullagah/service_1 ./Service_1
+docker build --no-cache -t paullagah/service_1 ./Service_1
 
 # Build Service 2 image 
-docker build -t paullagah/service_2 ./Service_2
+docker build --no-cache -t paullagah/service_2 ./Service_2
 
 # Build Service 3 image 
-docker build -t paullagah/service_3 ./Service_3
+docker build --no-cache -t paullagah/service_3 ./Service_3
 
 # Build Service 4 image -> print image list after build
 docker build --no-cache -t paullagah/service_4 ./Service_4
