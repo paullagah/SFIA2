7484b19532e9785472b73e35183a8adc075f5584
Paul Lagah <plagah93@gmail.com>
2020-07-29 09:48:46 +0000

    docker stack deploys without if statement

--- scripts/deploy_stack.sh
+++ scripts/deploy_stack.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-if [[ "$(docker stack services DnDservice 2> /dev/null)" == "" ]]; then
-    docker stack deploy --compose-file docker-compose.yaml DnDservice
-fi
+docker stack deploy --compose-file docker-compose.yaml DnDservice
 
