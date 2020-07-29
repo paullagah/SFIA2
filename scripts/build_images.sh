0fe1de4043b88e10be9ad06316ff3693887b39d0
Paul Lagah <plagah93@gmail.com>
2020-07-29 09:20:32 +0000

    made service update part of else statement

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -3,13 +3,17 @@
 # Print image list before build -> Build Service 1 image 
 echo $(docker images)
 docker build --no-cache -t paullagah/service_1 ./Service_1
+docker push paullagah/service_1:latest
 
 # Build Service 2 image 
 docker build --no-cache -t paullagah/service_2 ./Service_2
+docker push paullagah/service_2:latest
 
 # Build Service 3 image 
 docker build --no-cache -t paullagah/service_3 ./Service_3
+docker push paullagah/service_3:latest
 
 # Build Service 4 image -> print image list after build
 docker build --no-cache -t paullagah/service_4 ./Service_4
+docker push paullagah/service_4:latest
 echo $(docker images)
