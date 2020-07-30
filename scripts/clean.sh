f6bc538b5a24ad981a84556a4c80ddccf3076aa1
Paul Lagah <plagah93@gmail.com>
2020-07-30 11:51:50 +0000

    removed if statement

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-if [[ $(docker stack services DnDservice 2> /dev/null) != ""  ]];
 docker service update --image paullagah/service_1:latest DnDservice_service_1
 docker service update --image paullagah/service_2:latest DnDservice_service_2
 docker service update --image paullagah/service_3:latest DnDservice_service_3
 docker service update --image paullagah/service_4:latest DnDservice_service_4
 # docker rmi $(docker images -f "dangling=true" -q) -f
-fi
 
 docker system prune -f
