e02650b9d508edc108dbd06da2ca13ac01b01a16
Paul Lagah <plagah93@gmail.com>
2020-07-30 11:40:22 +0000

    install docker through playbook and main.yml

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -39,5 +39,5 @@ def home():
     response = requests.get('http://service_4:5003/merge')
     print(response)
     sentence = response.text
-    dnd_data = dnd.query.all()
+    # dnd_data = dnd.query.all()
     return render_template('index.html', sentence = sentence, dnd = dnd_data, title = 'Home')
