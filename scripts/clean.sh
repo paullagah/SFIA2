19e14085696d996c8d8a040bebd2d7f369863bdd
Paul Lagah <plagah93@gmail.com>
2020-07-30 09:47:22 +0000

    fixed if statement

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "docker stack services DnDservice 2> /dev/null" != ""  ]]
+if [[ "docker stack services DnDservice 2> /dev/null" != ""  ]];
 docker service update --image paullagah/service_1:latest DnDservice_service_1
 docker service update --image paullagah/service_2:latest DnDservice_service_2
 docker service update --image paullagah/service_3:latest DnDservice_service_3
