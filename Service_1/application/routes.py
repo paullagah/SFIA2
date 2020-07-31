f6520557cc658b3dbab889f98df8e75d00e10a06
Paul Lagah <plagah93@gmail.com>
2020-07-31 13:52:12 +0000

    added source for environment variables

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -30,7 +30,7 @@ class dnd(db.Model):
         [
             'Race: ' + self.races + '\n' 
             'Classes ' + self.classes + '\n'
-            'ID: ' + self.id
+            'ID: ' + str(self.id)
         ]
     )
 
@@ -39,5 +39,6 @@ class dnd(db.Model):
 def home():
     response = requests.get('http://service_4:5003/merge')
     print(response)
+    sentence = response.text
     dnd_data = dnd.query.all()
-    return render_template('index.html', sentence=response, dnd_data=dnd_data, title='Home')
+    return render_template('index.html', sentence=sentence, dnd=dnd_data, title='Home')
--- scripts/deploy_stack.sh
+++ scripts/deploy_stack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+source /var/lib/jenkins/.bashsrc
 docker stack deploy --compose-file docker-compose.yaml DnDservice
 
