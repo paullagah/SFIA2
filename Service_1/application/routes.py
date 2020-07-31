fcc51599ec1632927296cd4f00b93443f8590988
Paul Lagah <plagah93@gmail.com>
2020-07-31 13:01:30 +0000

    Adding database section into service 1 & 4

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -1,6 +1,7 @@
 from application import app
 from flask import render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
 import requests
 import random
 
@@ -25,19 +26,18 @@ class dnd(db.Model):
     classes = db.Column(db.String(50), nullable=False)
    
     def __repr__(self):
-    return ''.join(
+        return ''.join(
         [
             'Race: ' + self.races + '\n' 
             'Classes ' + self.classes + '\n'
-            'ID: ' + str(self.id) 
+            'ID: ' + self.id
         ]
     )
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     response = requests.get('http://service_4:5003/merge')
     print(response)
-    sentence = response.text
-    # dnd_data = dnd.query.all()
-    return render_template('index.html', sentence = sentence, dnd = dnd_data, title = 'Home')
+    dnd_data = dnd.query.all()
+    return render_template('index.html', sentence=response, dnd_data=dnd_data, title='Home')
